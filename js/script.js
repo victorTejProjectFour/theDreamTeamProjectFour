91891662a0abb520872463aa0db28eadaca61dda
Victor Wong <victorkaihong@gmail.com>
2020-06-10 17:16:37 -0400

    update function CataArray

--- js/script.js
+++ js/script.js
@@ -28,11 +28,33 @@ triviaApp.getCatNameUrl = function(){
         method: 'GET',
         datatype: 'json',
     }).then(function(res){
-        console.log(res);
+        console.log(triviaApp.CataArray(res.trivia_categories));
+        console.log(res.trivia_categories);
     })
 }
 
+const asd = ['1','2','3','4','5','6','7'];
 
+
+triviaApp.CataArray = function(catName){
+    const x = [];
+    
+    let loop = 3;
+    while(loop > 0){
+        let index = Math.floor(Math.random() * (catName.length - 1));
+        console.log(x[name] == catName[name]);
+        /* Ex. 0 to 2 */
+        /* console.log(Index); */
+        if (x.includes(index)){
+            index = Math.floor(Math.random() * (catName.length - 1));
+        }else{
+            x.push(catName[index]);
+            loop--;
+        }
+       /*  if (x.includes */
+    }
+    return x;
+}
 /* triviaApp.getAnotherUrl = () => {
     $.ajax({
         url: ''
@@ -42,8 +64,11 @@ triviaApp.getCatNameUrl = function(){
 
 
 triviaApp.init = () => {
-    triviaApp.getTriviaUrl(20,'easy');
-    triviaApp.getTriviaUrl(9,'easy');
+/*     triviaApp.getTriviaUrl(20,'easy');
+    triviaApp.getTriviaUrl(9,'easy'); */
+    triviaApp.getCatNameUrl();
+/*     triviaApp.CataArray(asd);
+    console.log(triviaApp.CataArray(asd)); */
 /*     triviaApp.getAnotherUrl(); */
 }
 $(document).ready(triviaApp.init());
