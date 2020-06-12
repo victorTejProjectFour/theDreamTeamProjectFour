383d6304c9e62b898b8b19a5247e139dacd9b4f0
Tej Lehal <tejlehal13@gmail.com>
2020-06-11 23:51:44 -0400

    Added js functionalities and Html

--- js/script.js
+++ js/script.js
@@ -3,100 +3,152 @@ const triviaApp = {};
 
 triviaApp.apiQuestionUrl = 'https://opentdb.com/api.php';
 triviaApp.apiCatUrl = 'https://opentdb.com/api_category.php';
+triviaApp.allQuestions = [];
+triviaApp.totalScore = 0;
 
 
-triviaApp.getTriviaUrl = function (userCategory, userDifficulty) {
-    const questionObj = $.ajax({
+// triviaApp.getTriviaUrl = function (userCategory, userDifficulty) {
+triviaApp.getTriviaUrl = function () {
+    const userCategory = (Math.floor(Math.random() * 23) + 9);
+    $.ajax({
         url: triviaApp.apiQuestionUrl,
         method: 'GET',
         datatype: 'json',
         data: {
-            amount: '3',
-            /* 20 */
+            amount: '9',
             category: userCategory,
-            /* easy */
-            difficulty: userDifficulty,
+            difficulty: 'easy',
             type: 'multiple'
         }
+    }).then(function (results) {
+        triviaApp.allQuestions.push(...results.results);
+        triviaApp.addBoolean();
+        console.log(triviaApp.allQuestions);
     })
-    return questionObj;
 }
 
-triviaApp.getCatNameUrl = function () {
-    return $.ajax({
-        url: triviaApp.apiCatUrl,
-        method: 'GET',
-        datatype: 'json',
-    })/* .then(function (res) {
-        console.log(triviaApp.CataArray(res.trivia_categories));
-        return triviaApp.CataArray(res.trivia_categories);
-        console.log(res.trivia_categories);
-    }) */
-}
+// triviaApp.getCatNameUrl = function () {
+//     return $.ajax({
+//         url: triviaApp.apiCatUrl,
+//         method: 'GET',
+//         datatype: 'json',
+//     })
+// }
 
-triviaApp.CataArray = function (catName) {
-    const categoryTitle = [];
-    do {
-        categoryTitle[categoryTitle.length] = catName.splice(Math.floor(Math.random() * catName.length), 1)[0];
-    } while (categoryTitle.length < 3)
-    return categoryTitle;
+triviaApp.addBoolean = function () {
+    triviaApp.allQuestions.forEach(function (item) {
+        item.boxStatus = true;
+    })
 }
+// triviaApp.CataArray = function (catName) {
+//     const categoryTitle = [];
+//     do {
+//         categoryTitle[categoryTitle.length] = catName.splice(Math.floor(Math.random() * catName.length), 1)[0];
+//     } while (categoryTitle.length < 3)
+//     return categoryTitle;
+// }
 
 // Create a Function that will have a parameter of an obj 
 triviaApp.displayQuestions = function (obj) {
-    console.log(obj);
+    $(`.multipleChoice`).empty();
     const choices = [...obj.incorrect_answers];
     let answerRandomIndex = Math.floor(Math.random() * (4 - 1)) + 1;
     // console.log(answerRandomIndex);
     choices.splice(answerRandomIndex - 1, 0, obj.correct_answer);
     // console.log(choices);
-    $('.questionSection').html(`
-        <h2>${obj.question}<h2>
-    `)
-    choices.forEach((choice, index) => {
-        $(`.questionSection`).append(`<p>${choice}</p>`)
+    $('.randomQuestion').text(obj.question);
+    choices.forEach((choice) => {
+        $(`.multipleChoice`).append(`<button class="multipleChoiceAnswer">${choice}</button>`)
+    })
+    triviaApp.checkUserAnswer(obj);
+}
+
+triviaApp.checkUserAnswer = function (obj) {
+    console.log('obj', obj);
+    $('.multipleChoiceAnswer').on('click', function () {
+        console.log('hello');
+        let userAnswer = $(this).text();
+        console.log(userAnswer);
+        console.log(obj.correct_answer);
+        if (userAnswer == obj.correct_answer) {
+            triviaApp.scoreCount(obj.points);
+            console.log(triviaApp.totalScore);
+        }
     })
 }
 
-// Create a function that displays the Categories
-triviaApp.displayCategories = function () {
+triviaApp.scoreCount = function (points) {
+    triviaApp.totalScore += points;
+}
 
+triviaApp.displayRightAnswer =
+
+    // Create a function that displays the Categories
+    triviaApp.displayCategories = function () {
+        // cat1questions = arr.filter(question => question.category === category[0].name)
+        // console.log(cat1questions);
+        let category = triviaApp.allQuestions[0].category;
+        $('.categoryName').text(category);
+        triviaApp.checkQuestionButton();
+    }
+
+triviaApp.checkQuestionButton = function () {
+    $('.jQuestion').on('click', function () {
+        // console.log(`${data - index}`);
+        $('.categorySection').hide();
+        let dataIndex = $(this).data("index");
+        // console.log($(this).text());
+        triviaApp.allQuestions[dataIndex].points = parseInt($(this).text());
+        triviaApp.allQuestions[dataIndex].boxStatus = false;
+        triviaApp.displayQuestions(triviaApp.allQuestions[dataIndex]);
+        $('.questionSection').fadeIn(1000);
+    })
 }
 
 //Extracting all 9 questions
-triviaApp.allQuestions = [];
-triviaApp.getAllQuestions = function(category){
-    
-    const difficulty = ['easy','medium','hard'];
-    for(let i = 0; i < category.length ; i++){
-            triviaApp.getTriviaUrl(category[i].id,'medium').then(function(rest){
-                /* console.log(...rest.results); */
-                triviaApp.allQuestions.push(...rest.results);
-                /* console.log(triviaApp.allQuestions.length); */
-            })
-    }
-/*     if(triviaApp.allQuestions.length < 9){
-        
-    } */
+
+// triviaApp.getAllQuestions = function (category) {
+
+//     const difficulty = ['easy', 'medium', 'hard'];
+//     for (let i = 0; i < category.length; i++) {
+//         triviaApp.getTriviaUrl(category[i].id, 'medium').then(function (rest) {
+//             triviaApp.allQuestions.push(...rest.results);
+//         })
+//     }
+// }
+
+triviaApp.starGame = function () {
+    $('.categorySection').hide();
+    $('.questionSection').hide();
+    $('.gameStart').on('click', function () {
+        console.log('hello');
+        $('header').hide();
+        $('.categorySection').fadeIn(1000);
+        triviaApp.displayCategories();
+
+    });
 }
 
 
 
 triviaApp.init = () => {
-    /*     triviaApp.getTriviaUrl(20,'easy');
-        triviaApp.getTriviaUrl(9,'easy'); */
-     triviaApp.getCatNameUrl().then(function(rest){
-        const x = triviaApp.CataArray(rest.trivia_categories);
-        triviaApp.getAllQuestions(x);
-    });
 
-     
-/*     triviaApp.getTriviaUrl(9, 'easy').then(function (result) {
-        triviaApp.displayQuestions(result.results[0]);
-    }) */
 
-    triviaApp.getAllQuestions(triviaApp.getCatNameUrl());
-    console.log(triviaApp.allQuestions);
+    triviaApp.starGame();
+    triviaApp.checkQuestionButton();
+    // triviaApp.getCatNameUrl().then(function (rest) {
+    //     const x = triviaApp.CataArray(rest.trivia_categories);
+    //     triviaApp.getAllQuestions(x);
+    // });
+
+    triviaApp.getTriviaUrl()
+
+    /*     triviaApp.getTriviaUrl(9, 'easy').then(function (result) {
+            triviaApp.displayQuestions(result.results[0]);
+        }) */
+
+    // triviaApp.getAllQuestions(triviaApp.getCatNameUrl());
+    // console.log(triviaApp.allQuestions);
     /* console.log(triviaApp.allQuestions); */
     /*     triviaApp.CataArray(asd);
         console.log(triviaApp.CataArray(asd)); */
